771350c21da0664bd1f0a21eec947ffec492b455
Louis Horrell <lhorrell99@gmail.com>
2023-11-08 14:17:16 +0000

    Into the weeds

---
+++ src/experience/assets.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    name: "base",
+    data: {},
+    items: [
+      {
+        name: "earthTopoBathyTexture",
+        source: "/assets/nasa-earth-topo-bathy-july-5400x2700.png",
+        type: "texture",
+      },
+    ],
+  },
+];
