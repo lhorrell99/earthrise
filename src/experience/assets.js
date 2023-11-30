08b8dafd4aa1c20f90b2f6b603f48a32f9ab3366
Louis Horrell <lhorrell99@gmail.com>
2023-11-30 19:50:08 +0000

    WIP Face coloring

--- src/experience/assets.js
+++ src/experience/assets.js
@@ -4,4 +4,14 @@ export default [
     source: "/assets/nasa-earth-topo-bathy-july-5400x2700.png",
     type: "image",
   },
+  {
+    name: "dGIVertices",
+    source: "/geometry/dualGeodesicIcosahedronVertices.json",
+    type: "json",
+  },
+  {
+    name: "dGIFaces",
+    source: "/geometry/dualGeodesicIcosahedronFaces.json",
+    type: "json",
+  },
 ];
--- src/experience/utils/resources.js
+++ src/experience/utils/resources.js
@@ -21,6 +21,9 @@ export default class Resources extends EventEmitter {
     this.loaders = {};
     // this.loaders.textureLoader = new THREE.TextureLoader();
     this.loaders.imageLoader = new THREE.ImageLoader();
+
+    this.loaders.jsonLoader = new THREE.FileLoader();
+    this.loaders.jsonLoader.setResponseType("json");
   }
 
   startLoading() {
@@ -32,7 +35,17 @@ export default class Resources extends EventEmitter {
       //   });
       // }
       if (asset.type === "image") {
-        this.loaders.imageLoader.load(asset.source, (file) => {
+        this.loaders.imageLoader.load(asset.source, (img) => {
+          const canvas = document.createElement("canvas");
+          const context = canvas.getContext("2d");
+          context.drawImage(img, img.width, img.height);
+
+          // Save canvas to items
+          this.assetLoaded(asset, canvas);
+        });
+      }
+      if (asset.type === "json") {
+        this.loaders.jsonLoader.load(asset.source, (file) => {
           this.assetLoaded(asset, file);
         });
       }
