a132d9de2ed293a8f5b8dca2442ffdee8391da76
Louis Horrell <lhorrell99@gmail.com>
2023-12-05 13:13:00 +0000

    Add some useful (temporary) textures

--- src/experience/assets.js
+++ src/experience/assets.js
@@ -1,6 +1,11 @@
 export default [
   {
     name: "earthTopoBathyImage",
+    // source: "/assets/nasaEarthBathy3600x1800.jpeg",
+    // source: "/assets/nasaEarthLandSeaIceTopo2048x1024.png",
+    // source: "/assets/nasaEarthNight2400x1200.jpeg",
+    // source: "/assets/nasaEarthNight3600x1800y2012.jpg",
+    // source: "/assets/nasaEarthNightLightsOnly.gif",
     source: "/assets/nasaEarthTopoBathyJuly5400x2700.png",
     type: "image",
   },
